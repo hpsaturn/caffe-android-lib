98a58159f06000f34f1b2d50dfb4a4c7a6bca0bb
Antonio Vanegas <hpsaturn@gmail.com>
2018-10-04 17:12:16 -0500

    libzmq ready via official build script (toolchain 4.9 and ndk10)

---
+++ scripts/build_libzmq.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -eu
+
+# shellcheck source=/dev/null
+. "$(dirname "$0")/../config.sh"
+
+ZEROMQ_DIR=libzmq/builds/android
+ZEROMQ=${PROJECT_DIR}/${ZEROMQ_DIR}
+ZEROMQ_BUILD_DIR=${ZEROMQ}/prefix
+ZEROMQ_HOME=${INSTALL_DIR}/zmq
+
+rm -rf "${ZEROMQ_BUILD_DIR}"
+mkdir -p "${ZEROMQ_BUILD_DIR}"
+pushd "${ZEROMQ_BUILD_DIR}"
+
+cd $ZEROMQ
+
+export ANDROID_NDK_ROOT=/opt/android-ndk-r10e
+export TOOLCHAIN_NAME=arm-linux-androideabi-4.9
+export TOOLCHAIN_HOST=arm-linux-androideabi
+export TOOLCHAIN_PATH=${ANDROID_NDK_ROOT}/toolchains/${TOOLCHAIN_NAME}/prebuilt/linux-x86_64/bin
+export TOOLCHAIN_ARCH=arm
+
+./build.sh
+
+rm -rf "${INSTALL_DIR}/zmq"
+cp -r ${ZEROMQ_BUILD_DIR}/${TOOLCHAIN_NAME} "${INSTALL_DIR}/zmq"
+
+popd
